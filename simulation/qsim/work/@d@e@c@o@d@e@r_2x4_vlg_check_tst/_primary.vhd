96c684396777944cc04321d63950352dc7af2212
Bernardo Silva Andrade <bernardo.102006@gmail.com>
2025-05-22 17:20:39 -0300

    Add files via upload

---
+++ simulation/qsim/work/@d@e@c@o@d@e@r_2x4_vlg_check_tst/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity DECODER_2x4_vlg_check_tst is
+    port(
+        o0              : in     vl_logic;
+        o1              : in     vl_logic;
+        o2              : in     vl_logic;
+        o3              : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end DECODER_2x4_vlg_check_tst;
