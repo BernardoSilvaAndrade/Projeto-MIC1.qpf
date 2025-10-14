cbe9ad0ec005f8f95c5ed538b05e990a8d01b257
João Vitor Lobato Romualdo <joaovitorlobatoromualdo16@gmail.com>
2025-10-14 12:35:37 -0300

    Delete Projeto-MIC1.qpf-main/Projeto-MIC1.qpf-main directory

--- Projeto-MIC1.qpf-main/Projeto-MIC1.qpf-main/simulation/qsim/work/@d@e@c@o@d@e@r_2x4_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity DECODER_2x4_vlg_sample_tst is
-    port(
-        i0              : in     vl_logic;
-        i1              : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end DECODER_2x4_vlg_sample_tst;
